095c45af1ee9efdf7ca6b99b1d39b0e74219f63f
alelax <alessandro.lausdei@gmail.com>
2018-05-30 21:57:16 +0200

    ex completato

--- add-guest.php
+++ add-guest.php
@@ -2,8 +2,8 @@
 
    $guest_id = $_POST['id_inserted'];
    $reservation_id = $_POST['subject'];
-   echo $guest_id;
-   echo $reservation_id;
+   // echo $guest_id;
+   // echo $reservation_id;
 
    $server = "localhost";
    $user = "root";
@@ -15,10 +15,35 @@
    if ($db_connection->connect_error) {
       echo "Connection failed " . $db_connection->connect_error;
    } else {
-      $reservation_query = "  INSERT INTO prenotazioni_has_ospiti(prenotazione_id, ospite_id, created_at, updated_at)
-                              VALUES ($reservation_id , $guest_id ,NOW(),NOW())";
+      $check_ospiteId_query = "  SELECT *
+                                 FROM ospiti
+                                 WHERE id = $guest_id";
 
-      $result = $db_connection->query($reservation_query);
+      $result_check_query = $db_connection->query($check_ospiteId_query);
+
+      $check_ospiteInBooking_query =   "SELECT *
+                                       FROM `prenotazioni_has_ospiti`
+                                       WHERE id = $reservation_id
+                                       AND ospite_id = $guest_id";
+
+      $result_check_query2 = $db_connection->query($check_ospiteInBooking_query);
+
+      if ($result_check_query->num_rows == 0 ) {
+         echo "L'Id inserito non è presente nel database";
+         die();
+
+      } elseif ($result_check_query2->num_rows > 0) {
+         echo "L'Id inserito fa già parte di questa prenotazione";
+         die();
+      }
+      else {
+         $reservation_query = "  INSERT INTO prenotazioni_has_ospiti(prenotazione_id, ospite_id, created_at, updated_at)
+         VALUES ($reservation_id , $guest_id ,NOW(),NOW())";
+
+         $result = $db_connection->query($reservation_query);
+
+         echo "L'ospite con id $guest_id è stato inserito correttamente nella prenotazione  $reservation_id";
+      }
 
    }
 ?>
